6d82519710dbf49ef8b8febbbf794ec1db34eefd
Noel Bundick <88900042+noelbundick-msft@users.noreply.github.com>
2022-04-27 20:51:55 +0000

    2 simple projects

---
+++ webapi/src/main/java/com/github/noelbundick_msft/apps/webapi/WebapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.noelbundick_msft.apps.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapiApplication.class, args);
+	}
+
+}
